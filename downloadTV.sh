e920ade8d58366bf847d898f4a2c1838acc8ee6f
dcman <dcman@users.noreply.github.com>
2018-11-16 17:22:09 -0800

    Added comment

--- downloadTV.sh
+++ downloadTV.sh
@@ -4,7 +4,10 @@ title=$1
 downloadUrl=$2
 apiKey=$3
 date=$(date -u +"%Y-%m-%d %H:%M:%SZ")
-
+##############
+# After a day or two of working you might want to stop the log, as it could get crazy big
+# To stop the logging add a # before the word echo two line down... Should look like #echo $date $title $downloadUrl $apiKey  >> ~/Movie.log
+##############
 echo $date $title $downloadUrl $apiKey  >> ~/TV.log
 
 {
