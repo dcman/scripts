56f0ba46de028c98d7892d987bcabf45b60ec931
dcman <dcman@users.noreply.github.com>
2018-11-16 17:21:17 -0800

    Create downloadMovie.sh

---
+++ downloadMovie.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+title=$1
+downloadUrl=$2
+apiKey=$3
+date=$(date -u +"%Y-%m-%d %H:%M:%SZ")
+##############
+# After a day or two of working you might want to stop the log, as it could get crazy big
+# To stop the logging add a # before the word echo two line down... Should look like #echo $date $title $downloadUrl $apiKey  >> ~/Movie.log
+##############
+echo $date $title $downloadUrl $apiKey  >> ~/Movie.log
+
+{
+/usr/bin/curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Api-Key: $apiKey" -X POST -d '{"title":"'"$title"'","downloadUrl":"'"$downloadUrl"'","downloadProtocol":"torrent","publishDate":"'"$date"'"}' http://localhost:11231/api/release/push
+} &> /dev/null
